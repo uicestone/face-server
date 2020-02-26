3b17135612ef5cb66b0e6419b3cbaf477cf8f122
uuz <ncysatnaf@gmail.com>
2020-02-26 12:44:00 +0800

    init permission

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -1,25 +1,74 @@
-import { rule, shield } from "graphql-shield"
+import { rule, shield, allow } from "graphql-shield"
 import { getUser } from "../utils"
+import { Context } from "../context"
 
 const rules = {
-  isAuthenticatedUser: rule()((parent, args, context) => {
-    const { userId } = getUser(context)
+  isAuthenticatedUser: rule()((parent, args, ctx: Context) => {
+    const { userId } = getUser(ctx)
     return Boolean(userId)
   }),
-  isPostOwner: rule()(async (parent, { id }, context) => {
-    const { userId } = getUser(context)
-    const author = await context.prisma.post
+  isCommunityManager: rule()(async (parent, { where: { id } }, ctx: Context) => {
+    const { userId } = getUser(ctx)
+    const community = await ctx.prisma.user.findOne({ where: { id: userId } }).community()
+    return id == community?.id
+  }),
+  isPostOwner: rule()(async (parent, { id }, ctx: Context) => {
+    const { userId } = getUser(ctx)
+    const author = await ctx.prisma.post
       .findOne({
         where: {
-          id,
-        },
+          id
+        }
       })
       .author()
-    return userId === author.id
-  }),
+    return userId === author?.id
+  })
 }
 
+//https://github.com/maticzav/graphql-shield
 export const permissions = shield({
-  Query: {},
-  Mutation: {},
+  Query: {
+    community: allow,
+    communities: allow,
+    resident: allow,
+    residents: allow,
+    passRecord: allow,
+    passRecords: allow,
+    unit: allow,
+    units: allow,
+    user: allow,
+    users: allow
+  },
+  Mutation: {
+    upsertOneCommunity: allow,
+    createOneCommunity: allow,
+    deleteOneCommunity: allow,
+    deleteManyCommunity: allow,
+    updateManyCommunity: allow,
+    updateOneCommunity: allow,
+    upsertOneResident: allow,
+    createOneResident: allow,
+    deleteOneResident: allow,
+    deleteManyResident: allow,
+    updateManyResident: allow,
+    updateOneResident: allow,
+    upsertOnePassRecord: allow,
+    createOnePassRecord: allow,
+    deleteOnePassRecord: allow,
+    deleteManyPassRecord: allow,
+    updateManyPassRecord: allow,
+    updateOnePassRecord: allow,
+    upsertOneUnit: allow,
+    createOneUnit: allow,
+    deleteOneUnit: allow,
+    deleteManyUnit: allow,
+    updateManyUnit: allow,
+    updateOneUnit: allow,
+    upsertOneUser: allow,
+    createOneUser: allow,
+    deleteOneUser: allow,
+    deleteManyUser: allow,
+    updateManyUser: allow,
+    updateOneUser: allow
+  }
 })
